4a204096d59a89c8415faefc7301f7023669b9b0
ArrayagoM <juanmartinarrayago@gmail.com>
2025-03-06 18:13:29 -0500

    problema de cors solucion 1

--- src/app.js
+++ src/app.js
@@ -33,7 +33,7 @@ app.use(
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', process.env.URL_ASSESS);
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
