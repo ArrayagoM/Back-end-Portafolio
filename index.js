44172f26e949729a9f8313ec365cdcebd4bb831b
ArrayagoM <juanmartinarrayago@gmail.com>
2025-06-30 10:25:25 -0300

    index.js

--- index.js
+++ index.js
@@ -4,22 +4,22 @@ const app = require('./src/app');
 const port = process.env.PORT || 5200;
 const { connectDB } = require('./src/config/db');
 
-function getLocalIP() {
-  const interfaces = os.networkInterfaces();
-  for (const iface of Object.values(interfaces)) {
-    for (const config of iface) {
-      if (config.family === 'IPv4' && !config.internal) {
-        return config.address;
-      }
-    }
-  }
-  return '127.0.0.1';
-}
+// function getLocalIP() {
+//   const interfaces = os.networkInterfaces();
+//   for (const iface of Object.values(interfaces)) {
+//     for (const config of iface) {
+//       if (config.family === 'IPv4' && !config.internal) {
+//         return config.address;
+//       }
+//     }
+//   }
+//   return '127.0.0.1';
+// }
 
 connectDB()
   .then(() => {
-    const localIP = getLocalIP();
-    app.listen(port, '0.0.0.0', async () => {
+    // const localIP = getLocalIP();
+    app.listen(port, async () => {
       // Inicializar el servicio de WhatsApp y asignarlo a app.locals
       console.log(`Servidor escuchando en:`);
       console.log(`→ http://localhost:${port}`);
